e565076ae248d42fad0a4c7fa2b7c5c8d5e5a17e
= <tim.lebel@gmail.com>
2014-02-13 08:39:28 -0800

    Passing basic tests - nonce issues seem to be resolved

--- test.js
+++ test.js
@@ -5,8 +5,26 @@ var coinbase = require('./lib/coinbase.js'),
 coinbase.key(creds.key);
 coinbase.secret(creds.secret);
 
+var options = {
+	url: 'https://coinbase.com/api/v1/account/balance'
+};
+
+coinbase.get(options, function (res, err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(res);
+	}
+});
+
 var options2 = {
-	url: 'https://coinbase.com/api/v1/addresses'
+	url: 'https://coinbase.com/api/v1/account/generate_receive_address',
+	json: {
+		'address': {
+			'callback_url': 'http://www.example.com/callback',
+			'label': 'Dalmation donations'
+		}
+	}
 };
 
 coinbase.post(options2, function (res, err) {
